ec38415d3b1eab07915b9c6be93839cd85249613
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-10-26 17:00:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function changed(type){
   console.log(type)
-  if(type === 'Imgur'){
+  if(type.value === 'Imgur'){
     new Imgur({
       path: document.getElementById('mainForm')
     })
   }
+  type.disabled = true;
+}
+
+function clearForm(){
+  let hostPick = document.getElementById('hostPick');
+  hostPick.reset();
+  hostPick.children[0].disabled = false;
+  let info = document.getElementById('info');
+  info.replaceChildren();
+  document.getElementById('mainForm').replaceChildren();
+  info.textContent='Пожалуйста, выберите хоста.';
 }
 
 class Imgur{
@@ -22,7 +33,7 @@ class Imgur{
       path: this.main,
       type: 'password',
       id: 'userID',
-      text: 'Введите ID пользователя',
+      text: 'Введите ID пользователя (Client ID)',
       complete: 'off',
       required: true
     })
@@ -31,6 +42,17 @@ class Imgur{
       path: this.main,
       type: 'submit',
       id: 'idSubmit'
+    })
+
+    document.getElementById('info').replaceChildren()
+    new List({
+      path: document.getElementById('info'),
+      text: `Отлично, теперь зайдите <a href='https://api.imgur.com/oauth2/addclient'>сюда</a>, и создайте новое приложение заполнив форму, испрользуя подсказки ниже:`,
+      arr: [
+        'Application name - имя вашего приложения. Назовите как сами того желаете, не суть.',
+        'Authorization type - выберите <b>OAuth 2 authorization with a callback URL</b>',
+        `Authorization callback URL - введите адрес ${document.location.href}callback/imgur выделив и скопировав его, или же нажав <button onclick='navigator.clipboard.writeText(document.location.href+"callback/imgur")'>сюда</button>, и тогда этот адрес скопируется в ваш буфер обмена`,
+        'Email - введите какую-либо почту. В идеале, от вашего Imgur аккаунта, наверное.']
     })
   }
 }
@@ -50,5 +72,25 @@ class Input{
     this.label=document.createElement('label');
     this.label.textContent=text;
     this.div.appendChild(this.label);
+  }
+}
+
+
+class List{
+  constructor({path, text, arr}){
+    this.main=document.createElement('ul');
+    this.main.innerHTML=text;
+    path.append(this.main);
+
+    this.list=arr.forEach(e => {
+      new Li(this.main, e)
+    })
+  }
+}
+class Li{
+  constructor(path, text){
+    this.li=document.createElement('li');
+    this.li.innerHTML=text;
+    path.appendChild(this.li);
   }
 }
