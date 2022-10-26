4d9259cbc2503c0e416b522ed1493c59a6dcb1ac
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-10-26 14:03:17 +0300

    Update index.js

--- callback/index.js
+++ callback/index.js
@@ -16,7 +16,7 @@ function sendData(){
   let form = document.getElementById('form');
   let a = [{access:form.children[0].children[0].value, refresh:form.children[1].children[0].value, album:form.children[2].children[0].value, host:form.children[3].children[0].value, time:time.local().toISO()}]
   // console.log(a);
-  if(a) navigator.clipboard.writeText(a).then(res => {
+  if(a) navigator.clipboard.writeText(JSON.stringify(a)).then(res => {
     console.log('Скопировано, десу.');
   })
 };
